- Changed EM class to be used as a wrapper to a problem specific class containing the log-likelihood - TODO: Fix the var_lower_bnd problem

scripts/EM_algorithm.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.special import logsumexp
 from types import FunctionType
-from sklearn.cluster import KMeans
 
 class EM_wrapper:
 
@@ -23,9 +22,9 @@ class EM_wrapper:
 
 class EM(EM_wrapper):
 
-    def __init__(self, log_like_fn, init_fn=None, mix_par_init=None, mix_weights_init=None, model_type='normal'):
+    def __init__(self, init_fn=None, mix_par_init=None, mix_weights_init=None, model_type='normal'):
         self.model_type = str.lower(model_type)
-        self.log_like_fn = log_like_fn
+        # self.log_like_fn = log_like_fn
         self.bic = None
         self.aic = None
         if isinstance(init_fn, FunctionType):
@@ -41,11 +40,11 @@ class EM(EM_wrapper):
         log_like_points = np.zeros((self.n_train, self.n_clusters))
         for i_cluster, par in enumerate(zip(*self.mix_par)):
             weight = self.weights[i_cluster]
-            log_like_points[:, i_cluster] = np.log(weight) + self.log_like_fn(self.X_train, par) + np.log(1e-16)
+            log_like_points[:, i_cluster] = np.log(weight) + self.log_like_fn(par) + np.log(1e-16)
         post = np.exp(log_like_points - logsumexp(log_like_points, axis=1)[:, None])
         return post, log_like_points.sum()
 
-    def m_step(self, post, var_lower_bnd=1e-3):
+    def m_step(self, post, var_lower_bnd=1e-5):
         mix_par = ()
         if self.model_type == 'normal':
             mu = np.dot(post.T, self.X_train) / post.sum(axis=0)[:, None]
@@ -95,7 +94,9 @@ class EM(EM_wrapper):
 
             var /= post.sum(axis=0)
 
-            var_lower_bnd = 1
+            #TODO: What going on here?
+            var_lower_bnd = 1e-0
+
             var[np.where(np.isnan(var))] = var_lower_bnd
             var = np.maximum(var_lower_bnd, var)
             sigma = np.sqrt(var)

scripts/examples/GMM_example.py
@@ -4,6 +4,13 @@ from EM_algorithm import EM
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+class EM_GMM(EM):
+
+    def log_like_fn(self, par):
+        x = self.X_train
+        return norm.logpdf(x, loc=par[0], scale=par[1]).squeeze()
+
+
 # Generate data
 n_dim, n_clusters_data = 1, 2
 n_train, n_test = 100, 100
@@ -40,10 +47,8 @@ for i_cluster in range(n_clusters):
 mix_sigma_init = np.maximum(1e-6, mix_sigma_init)
 mix_par_init = (mix_mu_init, mix_sigma_init)
 
-log_like_fn = lambda x, par: norm.logpdf(x, loc=par[0], scale=par[1]).squeeze()  # Set log-likelihood function based on mixture distribution
-
 # EM algorithm for a single initialization
-em = EM(mix_weights_init=mix_weights_init, mix_par_init=mix_par_init, log_like_fn=log_like_fn)
+em = EM_GMM(mix_weights_init=mix_weights_init, mix_par_init=mix_par_init)
 em.train(X_train, n_clusters=2, tol=1e-15)
 
 # Visualize result

scripts/examples/GMR_Table_example.py
@@ -7,6 +7,15 @@ from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 
+class EM_GMR_table(EM):
+
+    def log_like_fn(self, par):
+        x = self.X_train
+        y = self.X_train[:, -1]
+        y_hat = x[:, :-1].dot(par[0])
+        log_like = norm.logpdf(y, loc=y_hat, scale=par[1])
+        return log_like
+
 
 def init_fn(data, n_clusters, Z=None):
     x = data[:, 1].reshape(-1, 1)
@@ -27,10 +36,6 @@ def init_fn(data, n_clusters, Z=None):
     mix_weights_init = np.array([sum(Z == i_cluster) for i_cluster in range(n_clusters)]) / x.shape[0]
     return mix_par_init, mix_weights_init
 
-def log_like_fn(x, par):
-    y_hat = x[:, :-1].dot(par[0])
-    log_like = norm.logpdf(x[:, -1], loc=y_hat, scale=par[1])
-    return log_like
 
 def logistic_prediction(x, theta):
     x = np.c_[np.ones(x.shape[0]), x]
@@ -93,9 +98,8 @@ table_train = table[:n_train]
 table_test = table[n_train:]
 
 data = np.c_[X_train, y_train]
-em = EM(log_like_fn, init_fn=init_fn, model_type='linear')
+em = EM_GMR_table(init_fn=init_fn, model_type='linear')
 em.train(data, init_method='random', n_clusters=n_clusters_data, tol=1e-6)
-
 
 clf = LogisticRegression(random_state=0).fit(table_train, em.Z)
 
@@ -118,22 +122,22 @@ plt.plot(x[:, -1], y_model_estimated, color='r')
 plt.scatter(X_train[:, -1], em.Z, color='r')
 
 
-## Visualize result
-# fig = plt.figure()
-# colors = ['blue', 'red', 'orange', 'purple', 'magenta']
-# x_grid = np.linspace(-5, 5, 10)
-# x_grid = np.c_[np.ones_like(x_grid), x_grid]
-# for i_cluster in [i for i in range(em.n_clusters)]:
-#     y_hat = np.dot(x_grid, em.mix_par[0][i_cluster])
-#     y_lower = y_hat - 1.64 * em.mix_par[1][i_cluster]
-#     y_upper = y_hat + 1.64 * em.mix_par[1][i_cluster]
-#     plt.scatter(X_train[cluster_idx[:n_train] == i_cluster, -1], y_train[cluster_idx[:n_train] == i_cluster], color=colors[i_cluster])
-#     plt.plot(x_grid[:, -1], y_hat, color='k')
-#     plt.fill_between(x_grid[:, -1], y_lower, y_upper, alpha=0.3, color='green')
-# plt.xlabel('Independent variable', fontsize=14)
-# plt.ylabel('Dependent variable', fontsize=14)
-# beta_OLS = np.dot(np.dot(np.linalg.inv(np.dot(X_train.T, X_train)), X_train.T), y_train)
-# y_hat_OLS = np.dot(x_grid, beta_OLS)
-# plt.plot(x_grid[:, -1], y_hat_OLS, linewidth=4, color='k', linestyle='--', label='OLS solution')
-# plt.legend(fontsize=12)
+# Visualize result
+fig = plt.figure()
+colors = ['blue', 'red', 'orange', 'purple', 'magenta']
+x_grid = np.linspace(-5, 5, 10)
+x_grid = np.c_[np.ones_like(x_grid), x_grid]
+for i_cluster in [i for i in range(em.n_clusters)]:
+    y_hat = np.dot(x_grid, em.mix_par[0][i_cluster])
+    y_lower = y_hat - 1.64 * em.mix_par[1][i_cluster]
+    y_upper = y_hat + 1.64 * em.mix_par[1][i_cluster]
+    plt.scatter(X_train[cluster_idx[:n_train] == i_cluster, -1], y_train[cluster_idx[:n_train] == i_cluster], color=colors[i_cluster])
+    plt.plot(x_grid[:, -1], y_hat, color='k')
+    plt.fill_between(x_grid[:, -1], y_lower, y_upper, alpha=0.3, color='green')
+plt.xlabel('Independent variable', fontsize=14)
+plt.ylabel('Dependent variable', fontsize=14)
+beta_OLS = np.dot(np.dot(np.linalg.inv(np.dot(X_train.T, X_train)), X_train.T), y_train)
+y_hat_OLS = np.dot(x_grid, beta_OLS)
+plt.plot(x_grid[:, -1], y_hat_OLS, linewidth=4, color='k', linestyle='--', label='OLS solution')
+plt.legend(fontsize=12)
 

scripts/examples/GMR_example.py
@@ -6,6 +6,16 @@ from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 
 
+class EM_GMR(EM):
+
+    def log_like_fn(self, par):
+        x = self.X_train[:, :-1]
+        y = self.X_train[:, -1]
+        y_hat = x.dot(par[0])
+        log_like = norm.logpdf(y, loc=y_hat, scale=par[1])
+        return log_like
+
+
 def init_fn(data, n_clusters, Z=None):
     x = data[:, 1].reshape(-1, 1)
     y = data[:, -1]
@@ -24,11 +34,6 @@ def init_fn(data, n_clusters, Z=None):
     mix_par_init = (betas, sigma)
     mix_weights_init = np.array([sum(Z == i_cluster) for i_cluster in range(n_clusters)]) / x.shape[0]
     return mix_par_init, mix_weights_init
-
-def log_like_fn(x, par):
-    y_hat = x[:, :-1].dot(par[0])
-    log_like = norm.logpdf(x[:, -1], loc=y_hat, scale=par[1])
-    return log_like
 
 
 # Generate data
@@ -71,7 +76,7 @@ X_test = data_x[n_train:]
 y_test = data_y[n_train:]
 
 data = np.c_[X_train, y_train]
-em = EM(log_like_fn, init_fn=init_fn, model_type='linear')
+em = EM_GMR(init_fn=init_fn, model_type='linear')
 em.train(data, init_method='random', n_clusters=n_clusters_data, tol=1e-6)
 
 # Visualize result